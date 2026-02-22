fb60d757ff059de520bddc0f5f8083d2e607524a
Evku <evku39@gmail.com>
2026-02-22 18:02:27 +0200

    feat: added interactive changing coefs

--- examples/pid_regulator.py
+++ examples/pid_regulator.py
@@ -15,7 +15,7 @@ def regulator(x: float, target_x: float, v: float, step_integral: float) -> floa
 
     deviation = x - target_x
     integral += step_integral
-    return -(deviation * P_COEF + integral * I_COEF + v * D_COEF)
+    return -(deviation * regulator.p_coef + integral * regulator.i_coef + v * regulator.d_coef)
 
 
 def reset_callback() -> None:
@@ -24,6 +24,7 @@ def reset_callback() -> None:
     integral = 0.0
 
 
-sim = Sim(regulator, reset_callback=reset_callback, m=2.0, external_f=2.0, x0=1.0, freq=10.0, damping=0.5)
+sim = Sim(regulator, reset_callback=reset_callback, m=2.0, external_f=2.0, x0=1.0, freq=10.0, damping=0.5,
+          p_coef=P_COEF, i_coef=I_COEF, d_coef=D_COEF)
 sim_view = SimView(sim)
 sim_view.run()
